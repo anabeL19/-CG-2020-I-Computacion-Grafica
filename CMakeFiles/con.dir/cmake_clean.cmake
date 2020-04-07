52b0dcd656b873f3a1e42dfadda5580245ebfa91
anabeL19 <anabel.paredes@ucsp.edu.pe>
2020-04-06 19:06:08 -0500

    add library glad

---
+++ CMakeFiles/con.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/con.dir/src/First_program/con_glad.cpp.o"
+  "con"
+  "con.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/con.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/deps.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lebana/Documents/CompGraf/-CG-2020-I-Computacion-Grafica/first_program/con_glad.cpp" "/home/lebana/Documents/CompGraf/-CG-2020-I-Computacion-Grafica/CMakeFiles/deps.dir/first_program/con_glad.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_INCLUDE_NONE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "first_program"
+  "dependences/glfw-3.3.2/include"
+  "dependences/glad/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/lebana/Documents/CompGraf/-CG-2020-I-Computacion-Grafica/dependences/glfw-3.3.2/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/lebana/Documents/CompGraf/-CG-2020-I-Computacion-Grafica/CMakeFiles/glad.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
